6f50dc07c73df16662c98c62bd7ae6c95da283f7
Mehmet <81226436+mdizman@users.noreply.github.com>
2021-07-05 14:22:23 +0300

    SQLite Student Registration Example in C#

---
+++ ogrKayit/ogrKayit/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace ogrKayit
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtTC = new System.Windows.Forms.TextBox();
+            this.txtAD = new System.Windows.Forms.TextBox();
+            this.cmbSınıf = new System.Windows.Forms.ComboBox();
+            this.cmnBlm = new System.Windows.Forms.ComboBox();
+            this.btnKaydet = new System.Windows.Forms.Button();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.label1.Location = new System.Drawing.Point(118, 86);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(102, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "T.C Kimlik No :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.label2.Location = new System.Drawing.Point(118, 150);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "AD Soyad :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.label3.Location = new System.Drawing.Point(118, 213);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Sınıf AD :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.label4.Location = new System.Drawing.Point(118, 274);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(80, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Bölüm AD :";
+            // 
+            // txtTC
+            // 
+            this.txtTC.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtTC.Location = new System.Drawing.Point(280, 86);
+            this.txtTC.Name = "txtTC";
+            this.txtTC.Size = new System.Drawing.Size(369, 21);
+            this.txtTC.TabIndex = 4;
+            // 
+            // txtAD
+            // 
+            this.txtAD.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtAD.Location = new System.Drawing.Point(280, 150);
+            this.txtAD.Name = "txtAD";
+            this.txtAD.Size = new System.Drawing.Size(369, 21);
+            this.txtAD.TabIndex = 5;
+            // 
+            // cmbSınıf
+            // 
+            this.cmbSınıf.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbSınıf.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.cmbSınıf.FormattingEnabled = true;
+            this.cmbSınıf.Items.AddRange(new object[] {
+            "10. A",
+            "10. B",
+            "10. C",
+            "11. A",
+            "11. B",
+            "11. C",
+            "12. A",
+            "12. B",
+            "12. C"});
+            this.cmbSınıf.Location = new System.Drawing.Point(280, 213);
+            this.cmbSınıf.Name = "cmbSınıf";
+            this.cmbSınıf.Size = new System.Drawing.Size(369, 21);
+            this.cmbSınıf.TabIndex = 8;
+            // 
+            // cmnBlm
+            // 
+            this.cmnBlm.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmnBlm.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.cmnBlm.FormattingEnabled = true;
+            this.cmnBlm.Items.AddRange(new object[] {
+            "Bilişim Teknolojileri",
+            "Elektirik",
+            "İnşaat Teknolojileri",
+            "Dil"});
+            this.cmnBlm.Location = new System.Drawing.Point(280, 269);
+            this.cmnBlm.Name = "cmnBlm";
+            this.cmnBlm.Size = new System.Drawing.Size(369, 21);
+            this.cmnBlm.TabIndex = 9;
+            // 
+            // btnKaydet
+            // 
+            this.btnKaydet.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnKaydet.Location = new System.Drawing.Point(556, 353);
+            this.btnKaydet.Name = "btnKaydet";
+            this.btnKaydet.Size = new System.Drawing.Size(93, 23);
+            this.btnKaydet.TabIndex = 10;
+            this.btnKaydet.Text = "Kaydet";
+            this.btnKaydet.UseVisualStyleBackColor = true;
+            this.btnKaydet.Click += new System.EventHandler(this.btnKaydet_Click);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(280, 310);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.btnKaydet);
+            this.Controls.Add(this.cmnBlm);
+            this.Controls.Add(this.cmbSınıf);
+            this.Controls.Add(this.txtAD);
+            this.Controls.Add(this.txtTC);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ÖğrenciKayıt";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtTC;
+        private System.Windows.Forms.TextBox txtAD;
+        private System.Windows.Forms.ComboBox cmbSınıf;
+        private System.Windows.Forms.ComboBox cmnBlm;
+        private System.Windows.Forms.Button btnKaydet;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+    }
+}
+
---
+++ ogrKayit/ogrKayit/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SQLite;
+namespace ogrKayit
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnKaydet_Click(object sender, EventArgs e)
+        {
+            string yol = "Data source=dbornek.db";
+            SQLiteConnection baglanti = new SQLiteConnection(yol);
+            baglanti.Open();
+
+            string sql = "insert into tblOgrenciler(TcNo,AdSoyad,SinifAd,BolumAd,Tarih) values (@TcNo,@AdSoyad,@SinifAd,@BolumAd,@Tarih)";
+
+            SQLiteCommand komutIslet = new SQLiteCommand(sql, baglanti);
+            komutIslet.Parameters.AddWithValue("@TcNo", txtTC.Text);
+            komutIslet.Parameters.AddWithValue("@AdSoyad", txtAD.Text);
+            komutIslet.Parameters.AddWithValue("@SinifAd", cmbSınıf.Text);
+            komutIslet.Parameters.AddWithValue("@BolumAd", cmnBlm.Text);
+            komutIslet.Parameters.AddWithValue("@Tarih", dateTimePicker1.Value.ToString());
+
+            
+
+            komutIslet.ExecuteNonQuery();
+            baglanti.Close();
+            komutIslet.Dispose();
+            MessageBox.Show("Kayıt başarıyla gerçekleşti");
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
